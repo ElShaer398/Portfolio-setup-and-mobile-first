d41f9681be58a17c88751a26ca8f7139101c015f
Feliverse <114632980+Feliverse@users.noreply.github.com>
2022-12-19 17:07:17 -0400

    styling navbar

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ menuContainer.addEventListener("click", function () {
 
   if (!controller) {
     document.querySelector("header").style.background = "#6070ff";
+    document.querySelector("header").style.filter = "blur(50%)";
     document.querySelector(".main-1").style.background = "#6070ff";
+    document.querySelector(".main-1").style.filter = "blur(50%)"
     items.forEach(function(item) {
       item.style.color = "white";
+      item.style.fontSize = "32px";
+      item.style.fontWeight = "600";
+      item.style.marginBottom = "40px"
     })
     navItems.style.display = "flex";
     iconImage.src = "images/cancel.png";
@@ -19,11 +24,12 @@ menuContainer.addEventListener("click", function () {
     navItems.style.position = "absolute";
     navItems.style.left = "28px";
     navItems.style.top = "106px";
+    
+    
     controller = true;
   } else {
     navItems.style.display = "none";
     iconImage.src = "images/nav.png";
-    iconImage.style.background = "red"; //to remove later
     controller = false;
   }
 });
