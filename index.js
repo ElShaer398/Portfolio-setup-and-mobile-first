f9748f56a11b4e776ea35242995fe234b5976898
Mohammad Elshaer <melshaer398@gmail.com>
2022-12-19 22:38:51 +0200

    added styling

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ let controller = false;
 menuContainer.addEventListener("click", function () {
   const navItems = document.querySelector(".nav-items");
   const iconImage = menuContainer.querySelector("img");
+  const items = document.querySelectorAll(".items");
 
   if (!controller) {
+    document.querySelector("header").style.background = "#6070ff";
+    document.querySelector(".main-1").style.background = "#6070ff";
+    items.forEach(function(item) {
+      item.style.color = "white";
+    })
     navItems.style.display = "flex";
     iconImage.src = "images/cancel.png";
-    iconImage.style.background = "red"; //to remove later
+    navItems.style.flexDirection = "column";
+    navItems.style.listStyle = "none";
+    navItems.style.position = "absolute";
+    navItems.style.left = "28px";
+    navItems.style.top = "106px";
     controller = true;
   } else {
     navItems.style.display = "none";
