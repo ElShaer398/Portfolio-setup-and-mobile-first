0365fb9d8c17564363fe62a1d6eac3c05704e3c5
Mohammad Elshaer <melshaer398@gmail.com>
2023-01-03 16:33:20 +0200

    fixed linter errors

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const header = document.querySelector('header');
 const main1 = document.querySelector('.main-1');
 
 
-bars.addEventListener('click', function navBars() {
+bars.addEventListener('click', () => {
   popUpNav.style.transform = 'translateX(0px)';
   header.style.filter = 'blur(5px)';
   main1.style.filter = 'blur(5px)';
 });
 
-xMark.addEventListener('click', function navXMark() {
+xMark.addEventListener('click', () => {
   popUpNav.style.transform = 'translateX(-375px)';
   header.style.filter = 'blur(0px)';
   main1.style.filter = 'blur(0px)';
