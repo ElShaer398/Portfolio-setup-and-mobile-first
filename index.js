8d673fdec405336211838db306622dbb02b5c38f
Mohammad Elshaer <melshaer398@gmail.com>
2023-01-03 17:38:05 +0200

    added navbar animation

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const header = document.querySelector('header');
 const main1 = document.querySelector('.main-1');
 
 bars.addEventListener('click', () => {
-  popUpNav.style.transform = 'translateX(0px)';
+  popUpNav.style.visibility = 'visible';
+  popUpNav.style.opacity = '1';
   header.style.filter = 'blur(5px)';
   main1.style.filter = 'blur(5px)';
 });
 
 xMark.addEventListener('click', () => {
-  popUpNav.style.transform = 'translateX(-375px)';
+  popUpNav.style.visibility = 'hidden';
+  popUpNav.style.opacity = '0';
   header.style.filter = 'blur(0px)';
   main1.style.filter = 'blur(0px)';
 });
